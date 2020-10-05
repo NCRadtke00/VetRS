7613d78b695c9324784706fe460137fa5c1c6001
Jose <Sgtjosetorres@yahoo.com>
2020-10-05 18:54:10 -0500

    Corrected Syntax errors - SignalR working

--- VetRS/VetRS/Hubs/ChatHub.cs
+++ VetRS/VetRS/Hubs/ChatHub.cs
@@ -10,7 +10,7 @@ namespace VetRS.Hubs
     {
         public async Task SendMessage(string user, string message)
         {
-           await Clients.All.SendAsync("ReveiveMessage", user, message);
+           await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
 
     }
