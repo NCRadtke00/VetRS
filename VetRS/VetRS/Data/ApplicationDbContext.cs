810a0ce378b180012f4bb2b1782f3fdc7b6b39cb
Jose <Sgtjosetorres@yahoo.com>
2020-10-06 15:49:20 -0500

    added data to databases for vets/vso

--- VetRS/VetRS/Data/ApplicationDbContext.cs
+++ VetRS/VetRS/Data/ApplicationDbContext.cs
@@ -29,6 +29,150 @@ namespace VetRS.Data
                 NormalizedName = "ADMIN"
             }
             );
+            {
+                builder.Entity<VSO>().HasData(
+                    new VSO
+                    {
+                        Id = 1,
+                        FirstName = "Aaron",
+                        LastName = "Rodgers",
+                        PhoneNumber = "9205552020",
+                        Email = "Lombardibarbie@PackersChamps.com",
+                        ImageLocation = "",
+                        VSOStreet = "Martin Luther King Ave.",
+                        VSOCity = "Milwaukee",
+                        VSOState = "WI",
+                        VSOZipCode = 53022
+
+                    },
+                    new VSO
+                    {
+                        Id = 2,
+                        FirstName = "Brett",
+                        LastName = "Farve",
+                        PhoneNumber = "9203332020",
+                        Email = "Gunslinger@PackersChamps.com",
+                        ImageLocation = "",
+                        VSOStreet = "6th St.",
+                        VSOCity = "Milwaukee",
+                        VSOState = "WI",
+                        VSOZipCode = 53022
+                    },
+                    new VSO
+                    {
+                        Id = 3,
+                        FirstName = "Jordy",
+                        LastName = "Nelson",
+                        PhoneNumber = "5558675309",
+                        Email = "MrHands@PackersChamps.com",
+                        ImageLocation = "",
+                        VSOStreet = "31st St.",
+                        VSOCity = "Milwaukee",
+                        VSOState = "WI",
+                        VSOZipCode = 53022
+                    },
+                    new VSO
+                    {
+                        Id = 4,
+                        FirstName = "BJ",
+                        LastName = "Raji",
+                        PhoneNumber = "5552344545",
+                        Email = "Doubleyourdoublecheck@PackersChamps.com",
+                        ImageLocation = "",
+                        VSOStreet = "22nd St.",
+                        VSOCity = "Milwaukee",
+                        VSOState = "Wisconsin",
+                        VSOZipCode = 53022
+                    },
+                    new VSO
+                    {
+                        Id = 5,
+                        FirstName = "Bart",
+                        LastName = "Starr",
+                        PhoneNumber = "5558761515",
+                        Email = "Astarrisborn@packerschamps.com",
+                        ImageLocation = "",
+                        VSOStreet = "17th St.",
+                        VSOCity = "Milwaukee",
+                        VSOState = "WI",
+                        VSOZipCode = 53022
+                    }
+                 );
+
+
+
+            }
+            {
+                builder.Entity<Veteran>().HasData(
+                    new Veteran
+                    {
+                        Id = 1,
+                        FirstName = "Jose",
+                        LastName = "Torres",
+                        PhoneNumber = "9203827037",
+                        Email = "Sgtjosetorres@yahoo.com",
+                        ImageLocation = "",
+                        VeteranStreet = "515 Walnut St",
+                        VeteranCity = "Beaver Dam",
+                        VeteranState = "WI",
+                        VeteranZipCode = 53916
+                    },
+                    new Veteran
+                    {
+                        Id = 2,
+                        FirstName = "Eric",
+                        LastName = "Hill",
+                        PhoneNumber = "5552452010",
+                        Email = "Thatdude@softwaredeveloper.com",
+                        ImageLocation = "",
+                        VeteranStreet = "Kilbourn Ave",
+                        VeteranCity = "Milwaukee",
+                        VeteranState = "WI",
+                        VeteranZipCode = 53022
+                    },
+                    new Veteran
+                    {
+                        Id = 3,
+                        FirstName = "Chesty",
+                        LastName = "Puller",
+                        PhoneNumber = "1234567891",
+                        Email = "Realamericanhero@DevDogg.com",
+                        ImageLocation = "",
+                        VeteranStreet = "W. Wisconsin Ave.",
+                        VeteranCity = "Milwaukee",
+                        VeteranState = "WI",
+                        VeteranZipCode = 53022
+                    },
+                    new Veteran
+                    {
+                        Id = 4,
+                        FirstName = "Lucian",
+                        LastName = "Adams",
+                        PhoneNumber = "9201119999",
+                        Email = "MedalOfHonor@armyman.com",
+                        ImageLocation = "",
+                        VeteranStreet = "E. State St.",
+                        VeteranCity = "Milwaukee",
+                        VeteranState = "WI",
+                        VeteranZipCode = 53022
+                    },
+                    new Veteran
+                    {
+                        Id = 5,
+                        FirstName = "Steven",
+                        LastName = "Bennet",
+                        PhoneNumber = "9998883333",
+                        Email = "RealFlyBoy@airforcehero.com",
+                        ImageLocation = "",
+                        VeteranStreet = "E. Knapp St.",
+                        VeteranCity = "Milwaukee",
+                        VeteranState = "WI",
+                        VeteranZipCode = 53022
+                    }
+                    );
+
+            }
+
         }
     }
 }
