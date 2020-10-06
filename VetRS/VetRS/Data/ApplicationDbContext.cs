bbd13f120ff3900b015ab5ba115c8c364273b9b3
Kieran <Kieranherr@gmail.com>
2020-10-06 14:01:42 -0500

    Fixed DbSets

--- VetRS/VetRS/Data/ApplicationDbContext.cs
+++ VetRS/VetRS/Data/ApplicationDbContext.cs
@@ -14,9 +14,8 @@ namespace VetRS.Data
             : base(options)
         {
         }
-        public DbSet<Address> Address { get; set; }
         public DbSet<VSO> VSO { get; set; }
-        public DbSet<DOB> DOB { get; set; }
+
         public DbSet<Education> Education { get; set; }
         public DbSet<Veteran> Veteran { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
