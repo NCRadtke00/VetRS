f68cb8a6b064f85fa385490e61eb0499a416cdef
Nicholas Radtke <Raddnc@gmail.com>
2020-10-06 13:33:45 -0500

    Created display for Name in VSOView

--- VetRS/VetRS/Models/VSOView.cs
+++ VetRS/VetRS/Models/VSOView.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,9 @@ namespace VetRS.Models
 {
     public class VSOView
     {
+        [Display(Name = "Name")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
     }
 }
