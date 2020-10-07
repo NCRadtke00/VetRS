c35a1e5725499b4206291796eaee88d1bf4d2f7e
Kieran <Kieranherr@gmail.com>
2020-10-07 11:12:04 -0500

    Edited the VSOes controller and views to display a list veterans when a vso goes to the index. details button also works as it should

--- VetRS/VetRS/Controllers/VSOesController.cs
+++ VetRS/VetRS/Controllers/VSOesController.cs
@@ -22,7 +22,7 @@ namespace VetRS.Controllers
         // GET: VSOes
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.VSO.Include(v => v.IdentityUser);
+            var applicationDbContext = _context.Veteran.Include(v => v.IdentityUser);
             return View(await applicationDbContext.ToListAsync());
         }
 
@@ -34,15 +34,15 @@ namespace VetRS.Controllers
                 return NotFound();
             }
 
-            var vSO = await _context.VSO
+            var veteran = await _context.Veteran
                 .Include(v => v.IdentityUser)
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (vSO == null)
+            if (veteran == null)
             {
                 return NotFound();
             }
 
-            return View(vSO);
+            return View(veteran);
         }
 
         // GET: VSOes/Create
