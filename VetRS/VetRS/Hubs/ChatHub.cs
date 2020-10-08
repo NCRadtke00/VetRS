35b4758b30ce6856fd4cb33d1ab39fae609a816e
Nicholas Radtke <Raddnc@gmail.com>
2020-10-08 13:24:35 -0500

    fixing signalR

--- VetRS/VetRS/Hubs/ChatHub.cs
+++ VetRS/VetRS/Hubs/ChatHub.cs
@@ -12,6 +12,22 @@ namespace VetRS.Hubs
         {
            await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
 
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+        }
+        public Task SendPrivateMessage(string user, string message)
+        {
+            return Clients.User(user).SendAsync("ReceiveMessage", message);
+        }
     }
 }
