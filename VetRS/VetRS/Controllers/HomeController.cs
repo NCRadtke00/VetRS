6a8c90f9fc40723079698e1a1ba1ea180e6ffaf6
Nicholas Radtke <Raddnc@gmail.com>
2020-10-11 18:35:38 -0500

    Auto stash before merge of "main" and "origin/main"

--- VetRS/VetRS/Controllers/HomeController.cs
+++ VetRS/VetRS/Controllers/HomeController.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNet.SignalR;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
