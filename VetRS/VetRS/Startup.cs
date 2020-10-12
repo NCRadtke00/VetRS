6dc01aa0a50a0202dce139712e093bc463626d9b
Kieran <Kieranherr@gmail.com>
2020-10-11 19:22:22 -0500

    Eric's startup file

--- VetRS/VetRS/Startup.cs
+++ VetRS/VetRS/Startup.cs
@@ -2,15 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using VetRS.Hubs;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using VetRS.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Authentication.Cookies;
-
+using VetRS.Hubs;
+using System.Web.Routing;
+using Owin;
+using Microsoft.Owin;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using VetRS.ActionFilter;
+using Microsoft.AspNetCore.Http;
 namespace VetRS
 {
     public class Startup
@@ -19,145 +28,56 @@ namespace VetRS
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-            .AddCookie(options =>
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(
+            Configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            options.SignIn.RequireConfirmedAccount = false)
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultUI()
+            .AddDefaultTokenProviders();
+            services.AddScoped<ClaimsPrincipal>(s =>
+s.GetService<IHttpContextAccessor>().HttpContext.User);
+            services.AddControllers(config =>
             {
-                options.LoginPath = "/Join";
+                config.Filters.Add(typeof(GlobalRouting));
             });
-
-            services.AddRazorPages()
-                .AddRazorPagesOptions(options =>
-                {
-                    options.Conventions.AuthorizePage("/Index");
-                });
+            services.AddControllersWithViews();
+            services.AddRazorPages();
             services.AddSignalR();
+            services.AddMvc();
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthentication();
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
                 endpoints.MapHub<ChatHub>("/chathub");
             });
         }
     }
 }
-
-
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Builder;
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Identity.UI;
-//using Microsoft.AspNetCore.Hosting;
-//using Microsoft.AspNetCore.HttpsPolicy;
-//using Microsoft.EntityFrameworkCore;
-//using VetRS.Data;
-//using Microsoft.Extensions.Configuration;
-//using Microsoft.Extensions.DependencyInjection;
-//using Microsoft.Extensions.Hosting;
-//using VetRS.Hubs;
-//using System.Web.Routing;
-//using Owin;
-//using Microsoft.Owin;
-//using Microsoft.AspNetCore.Mvc;
-//using System.Security.Claims;
-//using VetRS.ActionFilter;
-//using Microsoft.AspNetCore.Http;
-
-//namespace VetRS
-//{
-//    public class Startup
-//    {
-//        public Startup(IConfiguration configuration)
-//        {
-//            Configuration = configuration;
-//        }
-
-//        public IConfiguration Configuration { get; }
-
-//        // This method gets called by the runtime. Use this method to add services to the container.
-//        public void ConfigureServices(IServiceCollection services)
-//        {
-//            services.AddDbContext<ApplicationDbContext>(options =>
-//            options.UseSqlServer(
-//            Configuration.GetConnectionString("DefaultConnection")));
-//            services.AddIdentity<IdentityUser, IdentityRole>(options =>
-//            options.SignIn.RequireConfirmedAccount = false)
-//            .AddEntityFrameworkStores<ApplicationDbContext>()
-//            .AddDefaultUI()
-//            .AddDefaultTokenProviders();
-//            services.AddScoped<ClaimsPrincipal>(s =>
-//s.GetService<IHttpContextAccessor>().HttpContext.User);
-//            services.AddControllers(config =>
-//            {
-//                config.Filters.Add(typeof(GlobalRouting));
-//            });
-//            services.AddControllersWithViews();
-//            services.AddRazorPages();
-//            services.AddSignalR();
-//            services.AddMvc();
-//        }
-
-//        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-//        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-//        {
-//            if (env.IsDevelopment())
-//            {
-//                app.UseDeveloperExceptionPage();
-//                app.UseDatabaseErrorPage();
-//            }
-//            else
-//            {
-//                app.UseExceptionHandler("/Home/Error");
-//                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//                app.UseHsts();
-//            }
-//            app.UseHttpsRedirection();
-//            app.UseStaticFiles();
-
-//            app.UseRouting();
-
-//            app.UseAuthentication();
-//            app.UseAuthorization();
-
-//            app.UseEndpoints(endpoints =>
-//            {
-//                endpoints.MapControllerRoute(
-//                    name: "default",
-//                    pattern: "{controller=Home}/{action=Index}/{id?}");
-//                endpoints.MapRazorPages();
-//                endpoints.MapHub<ChatHub>("/chathub");
-//            });
-//        }
-//    }
-//}
