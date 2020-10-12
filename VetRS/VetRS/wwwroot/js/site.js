7690f95d46336a09100301d0561c36d0a9abbb24
Kieran <Kieranherr@gmail.com>
2020-10-11 23:13:53 -0500

    Added googlemaps search function.

--- VetRS/VetRS/wwwroot/js/site.js
+++
@@ -1,9 +0,0 @@
-﻿var pusher = new Pusher('a8e8c58dc5d53b967a82', {
-    cluster: 'us3',
-    encrypted: true
-});
-
-var channel = pusher.subscribe('my-channel');
-channel.bind('my-event', function (data) {
-    reloadGroup();
-});
